cb6f0b9abca6e452646331d51f43aebba959e3f5
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-11-14 11:17:07 -0800

    Simple shell mini exercises

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 0x16. C - Simple Shell
+# Simple Shell
+This repository contains the creation of a unix command line interpreter that handles a series of commands. The shell is a program that interacts with the user through a terminalor takes the input of a file and executes a sequence of commands that are passed to the operating system. The shell is expected to work exactly like the thompson shell. The shell program must have the same output as sh(bin/sh), as well as the same error output. 
--- exercises/fork_wait_execve.c
+++ exercises/fork_wait_execve.c
@@ -1,12 +1,87 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "shell.h"
 
 int main(void)
 {
-	char *argv[] = {"ls", "-l", NULL};
-	execve("./tmp", argv, NULL);
+	printf("PID before first fork: %d\n", getpid());
+	char *argv[] = {"/usr/bin/ls", "-l", "/tmp", NULL};
+	pid_t child_pid;
 
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return (-1);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error:");
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return (-1);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error:");
+		}
+	}
+	else
+		wait(NULL);
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return (-1);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error:");
+		}
+	}
+	else
+		wait(NULL);
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return (-1);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error:");
+		}
+	}
+	else
+		wait(NULL);
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return (-1);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error:");
+		}
+	}
+	else
+		wait(NULL);
 	return (0);
 }
